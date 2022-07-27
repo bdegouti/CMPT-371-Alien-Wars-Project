3d0de9ae1eab414cb1ff20c7a56115ca5e4c3c27
Yosup Cheon <cheonyo0409@gmail.com>
2022-07-27 16:58:48 -0700

    Added client socket and API example

--- clientBackend.c
+++ clientBackend.c
@@ -3,7 +3,50 @@
 #include <unistd.h>
 #include <stdlib.h>
 
+#include <arpa/inet.h>
+#include <sys/socket.h>
+
+#include <stdio.h>
+
 #define BUF_SIZE 100
+#define DATA_PAYLOAD 255
+
+////////////////////
+////API Example/////
+////////////////////
+enum ttype {
+    conn = 10,
+    act = 20,
+    over = 30
+};
+
+typedef struct Message {
+	int direct; // 0 for S->C 1 for C->S
+    enum ttype type; //types of data in enum
+    char message[DATA_PAYLOAD]; //data that contains action or gamestate
+}message; 
+////////////////////
+////API Example/////
+////////////////////
+
+//connect client to server
+int clientSocket (int portN) {
+    int cltSock;
+    struct sockaddr_in cltAdr;
+
+    if((cltSock = socket(PF_INET, SOCK_STREAM, 0)) == -1){ printf("error sock");}
+
+    memset(&cltAdr, 0, sizeof(cltAdr));
+    cltAdr.sin_family = AF_INET;
+    // this connects to the localhost
+    // if user specify the ip then change is needed
+    cltAdr.sin_addr.s_addr = htonl(INADDR_ANY);
+    cltAdr.sin_port = htons((portN));
+
+    if(connect(cltSock, (struct sockaddr*)&cltAdr, sizeof(cltAdr)) == -1) {printf("error connect");}
+    return cltSock; //returns the socket
+}
+
 
 // send and receive the game state from the server
 char* clientServer (int cltSock, char * ret) {
@@ -16,4 +59,9 @@ char* clientServer (int cltSock, char * ret) {
     strcpy (gameState, buffer);
 
    return gameState;
+}
+
+//close socket
+void closeSocket (int cltSock) {
+    close(cltSock);
 }
--- clientBackend.h
+++ clientBackend.h
@@ -1,7 +1,12 @@
 #ifndef _clientBackend_h
 #define _clientBackend_h
 
+// create a client socket
+int clientSocket (int portN);
+
 // send and receive the game state from the server
 char* clientServer (int cltSock, char * ret);
 
+// close client side socket
+void closeSocket(int cltSockt);
 #endif
