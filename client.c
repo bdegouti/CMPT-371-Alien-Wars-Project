a6d2dcdf526d962b69ff4907d7c0dbfa53def84a
Mike Ostrowka <mikeostrowka@gmail.com>
2022-08-03 16:04:56 -0700

    Moved userInput to its own file and added Makefile

--- client.c
+++ client.c
@@ -4,87 +4,14 @@
 #include <unistd.h>
 #include <sys/types.h>
 #include <stdbool.h>
-
-//enum for console colors
-enum theme {
-    white,
-    red,
-    blue,
-    green
-};
-
-//theme codes for console colors
-const char *THEME_CODES[5] = {"\033[37m",
-                              "\033[31m",
-                              "\033[34m",
-                              "\033[32m"};
-const char *CLEAR_CODE = "\033[0m"; //clears all console formatting
-
-const char INPUT_ERROR[] = "Please enter a valid move!\nMoves are att, def, gun\n\n";
-
-const char* ATT = "att";
-const char* DEF = "def";
-const char* GUN = "gun";
+#include "userInput.h"
 
 bool gameOver = false; // true when game over
-
-
-struct PlayersInfo {
-    // id's range from 1 to 4.
-    char player; // id of the client
-    char ally;  // id of the client's ally
-    char enemy1; // id of the clients enemy
-    char enemy2; // id of the clients enemy
-};
-
-// gets user input
-// checks for user input error
-// returns valid user move 
-char* getUserInput(struct PlayersInfo playersInfo) {
-    char buffer[7]; // input size of 5 + \n + \0
-    char* ret = (char *) malloc(6); // 5 + \0
-
-    // loop till input valid 
-    bool input_not_valid = true;
-    while (input_not_valid) {
-        // get user input
-        printf("%s<move> <player target> %sEnter your move: %s", THEME_CODES[blue], THEME_CODES[green], THEME_CODES[red]);
-        fgets(buffer, 7, stdin);
-        buffer[5] = '\0'; //remove the \n from string 
-        printf("%s", CLEAR_CODE); // clear text formatting
-
-        // check user input valid 
-        if (strncmp(buffer, ATT, 3) == 0) { // Attack
-            if (buffer[4] == playersInfo.enemy1 || buffer[4] == playersInfo.enemy2) {
-                strcpy(ret, buffer);
-                input_not_valid = false;
-            } else {
-                printf("Invalid Player Target!\n");
-            }
-        } else if (strncmp(buffer, DEF, 3) == 0) { // Defense
-            if (buffer[4] == playersInfo.player || buffer[4] == playersInfo.ally) {
-                strcpy(ret, buffer);
-                input_not_valid = false;
-            } else {
-                printf("Invalid Player Target!\n");
-            }
-        } else if (strncmp(buffer, GUN, 3) == 0) { // Loading Gun
-            strcpy(ret, GUN);
-            input_not_valid = false;
-        } 
-        
-        if (input_not_valid) {
-            printf("%s is not a valid move.\n", buffer);
-            printf("%s%s%s", THEME_CODES[red], INPUT_ERROR, CLEAR_CODE); //print error and moves
-        }
-    }
-
-    return ret; 
-}
 
 // TODO: Client Server
 bool send_to_server(char * valid_move) {
     printf("%s\n", valid_move);
+    return true; //temporary return
 }
 
 // TODO: Client Server 
